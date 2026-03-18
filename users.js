77f6f2565f4d4494a8713bd55aab159b4f59274a
MatanSimon1 <96584976+MatanSimon1@users.noreply.github.com>
2026-03-18 12:40:25 +0200

    Update users.js

--- users.js
+++ users.js
@@ -1,3 +1,3 @@
 // ── הגדרות מערכת ───────────────────────────────────────────────────────────
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzF1EKN09oQuh6m52bkQ3FuZe-RjL0z5Z72jbddxvOyf1rgeRJmzTtWgGBgDLRXMvFw/exec';
-const ADMIN_PASSWORD = 'metricminds2024';
+const ADMIN_PASSWORD = 'Matan2504';
