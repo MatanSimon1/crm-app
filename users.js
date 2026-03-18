b33215d5f646bed37ff9ef2c1211a8659d3463f1
MatanSimon1 <96584976+MatanSimon1@users.noreply.github.com>
2026-03-18 11:04:28 +0200

    Delete users.js

--- users.js
+++
@@ -1,3 +0,0 @@
-// ── הגדרות מערכת ───────────────────────────────────────────────────────────
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx-3KUPO9V7IowwLd19Ty0X19xbhQQua0jEENSPjHln7S1xVK9T6rDl-aTOL8oJWFgI/exec';
-const ADMIN_PASSWORD = 'metricminds2024';
