72366c7f36e7da2a3f16ef9447d28e2623c4c933
MatanSimon1 <96584976+MatanSimon1@users.noreply.github.com>
2026-03-18 11:06:38 +0200

    Add files via upload

---
+++ users.js
@@ -0,0 +1,3 @@
+// ── הגדרות מערכת ───────────────────────────────────────────────────────────
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx-3KUPO9V7IowwLd19Ty0X19xbhQQua0jEENSPjHln7S1xVK9T6rDl-aTOL8oJWFgI/exec';
+const ADMIN_PASSWORD = 'metricminds2024';
