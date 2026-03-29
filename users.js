ca235a0f6eb8180cef99e8338df743587e47a68d
MatanSimon1 <96584976+MatanSimon1@users.noreply.github.com>
2026-03-29 12:39:19 +0300

    Update users.js

--- users.js
+++ users.js
@@ -1,5 +1,5 @@
 // ── הגדרות מערכת ───────────────────────────────────────────────────────────
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzF1EKN09oQuh6m52bkQ3FuZe-RjL0z5Z72jbddxvOyf1rgeRJmzTtWgGBgDLRXMvFw/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwXy1DDOccFkBc49QxayR1H-wHBELNf_mYq54mOdbs-yrakjm3xa1wQkBMtYJJgNukmwg/exec';
 
 // ── אדמינים ─────────────────────────────────────────────────────────────────
 // להוסיף אדמין: הוסף שורה { u:'username', p:'password' }
